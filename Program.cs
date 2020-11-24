8d4a2de4d999b42a2335fd156bbbe2a0bc45272f
Martin Bednorz <m.s.bednorz@gmail.com>
2020-11-24 11:48:29 +0100

    Initial import

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_my_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
